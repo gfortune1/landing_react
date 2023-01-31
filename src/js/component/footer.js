160516831af709d995587df9688519c24a1bc238
gfortune1 <fgeee1@gmail.com>
2023-01-31 02:04:43 +0000

    final

---
+++ src/js/component/footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+const Footer=()=>{
+    return(
+        <div className="bg-dark text-light text-center py-5">
+           copyright &copy; 2023
+        </div>
+    )
+}
+export default Footer
